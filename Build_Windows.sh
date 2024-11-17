ab94358d93ade06d33b0ed1086429bf520b9a297
JackoboLeChocobo <imbert.jacquesolivier@gmail.com>
2024-11-17 21:18:21 +0100

    Update #2

--- Build_Windows.sh
+++ Build_Windows.sh
@@ -3,4 +3,3 @@
 i686-w64-mingw32-gcc main.c -I./windows-deps/include -L./windows-deps/lib -lserialport -lmingw32 -mwindows -o GB_Duino.exe
 mkdir Windows_Build
 cp GB_Duino.exe ./Windows_Build/
-cp ./windows-deps/bin/*.dll ./Windows_Build
